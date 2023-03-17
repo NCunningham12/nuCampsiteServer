b6a02042c03269834e127785bc61765f06d45c7a
Nathan Cunningham <counterweight22@gmail.com>
2023-03-16 23:53:16 -0700

    Fixed Post endpoint for /:campsiteId

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -8,7 +8,7 @@ favoriteRouter
     .route('/')
     .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
     .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
-        Favorite.find({ user: req.user._id })
+        Favorite.find({ user: req.body._id })
             .populate('user')
             .populate('campsites')
             .then((favorites) => {
@@ -72,31 +72,43 @@ favoriteRouter
     );
 
 favoriteRouter
-    .route('./:campsiteId')
+    .route('/:campsiteId')
+    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
     .get(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
         res.statusCode = 403;
         res.end('GET operation not supported on /favorites/:campsiteId');
     })
     .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-        Favorite.findOne({ user: req.body._id })
+        Favorite.findOne({ user: req.user._id })
             .then((favorite) => {
                 if (favorite) {
-                    req.body.forEach((campsite) => {
-                        if (!favorite.campsites.includes(campsite)) {
-                            favorite.campsites.push(campsite);
-                        }
-                    });
-                    favorite.save().then((favorite) => {
-                        res.statusCode = 200;
-                        console.log('Favorite Updated', favorite);
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json(favorite);
-                    });
+                    if (!favorite.campsites.includes(req.params.campsiteId)) {
+                        favorite.campsites.push(req.params.campsiteId);
+
+                        favorite.save().then((favorite) => {
+                            res.statusCode = 200;
+                            console.log('Favorite Updated', favorite);
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favorite);
+                        });
+                    } else {
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.end(
+                            `That campsite is already in the list of favorites!`
+                        );
+                    }
                 } else {
-                    res.setHeader('Content-Type', 'text/plain');
-                    res.end(
-                        `That campsite is already in the list of favorites!`
-                    );
+                    Favorite.create({
+                        campsites: [req.params.campsiteId],
+                        user: req.user._id,
+                    }).then((favorite) => {
+                        favorite.save().then((favorite) => {
+                            res.statusCode = 200;
+                            console.log('Favorite Created', favorite);
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favorite);
+                        });
+                    });
                 }
             })
             .catch((err) => next(err));
